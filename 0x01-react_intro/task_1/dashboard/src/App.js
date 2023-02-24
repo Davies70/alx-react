9ff1523c5af3b03cffd6ed8741194816ef16829a
Ajayi Davies <ajayidavies7@gmail.com>
2023-02-24 23:46:09 +0100

    task 1

---
+++ 0x01-react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,18 @@
+import logo from './holberton_logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">Login to access the full dashboard</div>
+      <div className="App-footer">Copyright {getFullYear()} - {getFooterCopy(true)}</div>
+    </div>
+  );
+}
+
+export default App;
