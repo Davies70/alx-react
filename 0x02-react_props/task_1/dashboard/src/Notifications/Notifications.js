f5e99ac35d0f55ba344e842d4a52c56d54af0fa6
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-03 14:13:14 +0100

    close image icon made smaller

--- 0x02-react_props/task_1/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_1/dashboard/src/Notifications/Notifications.js
@@ -4,43 +4,45 @@ import { getLatestNotification } from '../utils/utils';
 import closeIcon from '../assets/close-icon.png';
 
 function Notifications() {
+  const buttonStyles = {
+    color: '#3a3a3a',
+    fontWeight: 'bold',
+    background: 'none',
+    border: 'none',
+    position: 'absolute',
+    right: '3px',
+    top: '3px',
+    cursor: 'pointer',
+    outline: 'none',
+  };
+
   return (
     <div className="Notifications">
-      <button style={{
-        color: '#3a3a3a',
-        fontWeight: 'bold',
-        background: 'none',
-        border: 'none',
-        fontSize: '15px',
-        position: 'absolute',
-        right: '3px',
-        top: '3px',
-        cursor: 'pointer',
-        outline: 'none',
-      }}
-      aria-label="Close"
-      onClick={(e) => {
-        console.log('Close button has been clicked');
-      }}
+      <button
+        style={buttonStyles}
+        aria-label="Close"
+        onClick={(e) => {
+          console.log('Close button has been clicked');
+        }}
       >
         <img src={closeIcon} alt="close icon" />
       </button>
       <p>Here is the list of notifications</p>
       <ul>
-        <li data='default'>
-					New course available
-				</li>
-				<li data='urgent'>
-					New resume available
-				</li>
-				<li
-					data='urgent'
-					dangerouslySetInnerHTML={{__html: getLatestNotification()}}
-				>
-				</li>
+        <li data="default">
+          New course available
+        </li>
+        <li data="urgent">
+          New resume available
+        </li>
+        <li
+          data="urgent"
+          dangerouslySetInnerHTML={{__html: getLatestNotification()}}
+        />
       </ul>
     </div>
   );
-};
+}
+
 
 export default Notifications;
