47d05274a7a9f8755dd368c18c68d972af919cee
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-17 13:04:46 +0100

    binded methods

--- 0x05-react_state/task_0/dashboard/src/App/App.js
+++ 0x05-react_state/task_0/dashboard/src/App/App.js
@@ -14,6 +14,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { displayDrawer: false };
+
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
   }
 
   handleDisplayDrawer = () => {
