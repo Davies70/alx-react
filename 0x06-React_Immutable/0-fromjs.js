21b0c45be82054642c53229bb5e6553a3a3fc54b
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-21 14:23:23 +0100

    made object the return value

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,8 +1,17 @@
-import { fromJS } from 'immutable';
+#!/usr/bin/node
+
+const { fromJS } = require('immutable');
 
 const getImmutableObject = (object) => {
-  const immutableObject = fromJS(object);
-  return immutableObject;
+  const object = fromJS(object);
+  return object;
 };
 
-getImmutableObject(object);
+const object = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132,
+};
+
+console.log(getImmutableObject(object));
