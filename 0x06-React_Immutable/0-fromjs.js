f02aa1cf17d861fdfdd781c1ab87aa661f609cba
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-21 15:02:59 +0100

    forgot to export

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -2,15 +2,6 @@
 
 import { fromJS } from 'immutable';
 
-const getImmutableObject = (object) => {
-  return fromJS(object);
-};
-
-const testObj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
-};
-
-getImmutableObject(testObj);
+export default function getImmutableObject(object) {
+  fromJS(object);
+}
