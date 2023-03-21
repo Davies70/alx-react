b8b7efe4969af34f88f752fab3974c4a075969c6
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-21 21:21:19 +0100

    used list instead

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -5,5 +5,6 @@ export function concatElements(page1, page2) {
 }
 
 export function mergeElements(page1, page2) {
-  return Map(page1).merge(Map(page2));
+  const newMap = Map(page1).merge(Map(page2));
+  return List(newMap);
 }
