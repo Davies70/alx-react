0fd6068acea8479213b5a774e82f710595fb141b
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-22 00:06:00 +0100

    linter checked

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -6,11 +6,12 @@ export default function printBestStudents(object) {
   const lazySeq = Seq(object);
   const filteredgrades = lazySeq.filter((grade) => grade.score > 70);
   const transformedGrades = filteredgrades.map((grade) => {
-    return {
+    const value = {
       ...grade,
       firstName: grade.firstName[0].toUpperCase() + grade.firstName.slice(1),
       lastName: grade.lastName[0].toUpperCase() + grade.lastName.slice(1),
     };
+    return value;
   });
   console.log(transformedGrades.toObject());
 }
