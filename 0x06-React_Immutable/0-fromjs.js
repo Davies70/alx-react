7feb337eb97a757b8c04eb00b69b5578ae85d7c2
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-21 13:51:24 +0100

    called the function

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -4,3 +4,12 @@ const getImmutableObject = (obj) => {
   const immutableObject = fromJS(obj);
   return immutableObject;
 };
+
+const object = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132,
+};
+
+getImmutableObject(object);
