d303a83b1a74795ef0dcebf8cf23335082fd0f6d
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-21 13:45:15 +0100

    used import

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,15 +1,6 @@
-const { fromJS } = require('immutable');
+import { fromJS } from 'immutable';
 
 const getImmutableObject = (obj) => {
   const immutableObject = fromJS(obj);
   return immutableObject;
 };
-
-const object = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
-};
-
-console.log(getImmutableObject(object));
