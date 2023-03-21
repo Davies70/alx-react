4e5f265e97855e6797a73c859350829e67c5ec07
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-21 19:03:59 +0100

    undefined

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,12 +1,5 @@
 import { Map } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const value = Map(object).getIn(array);
-
-  if (typeof value === 'string') {
-    return value;
-  } else if (Map.isMap(value)) {
-    return value;
-  }
-  return value === undefined ? undefined : value.toJS();
+  return Map(object).getIn(array, undefined);
 }
