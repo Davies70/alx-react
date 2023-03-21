1b675e84a2cb82f649e999e48e7196fbc207210a
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-21 19:58:41 +0100

    chained mutations

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,14 +1,14 @@
 import { Map } from 'immutable';
 
-const object = {
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
   4: 'Oliver',
   5: 'Jacob',
   6: 'Lucas',
-};
-export const map = Map(object);
-export const map2 = map.withMutation(mutableMap => {
-    mutableMap.set([2, 4], ['Benjamin', 'Oliver']);
-})
+});
+
+export const map2 = map.withMutations((values) => {
+  values.set(2, 'Benjamin').set(4, 'Oliver');
+});
