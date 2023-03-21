5ec3be71aa99cfcbec468f8b0a6d811f7fad9f7a
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-21 18:19:01 +0100

    try and catch

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,5 +1,9 @@
 import { Map } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  return Map(object).getIn(array);
+  let value;
+  try {
+    value = Map(object).getIn(array);
+  } catch (e) {}
+  return value;
 }
