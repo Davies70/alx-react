9d085a7ef9d6f4a8bd86c22f3ebc1c0016476c73
Davies70 <ajayidavies7@gamil.com>
2023-03-21 13:49:17 +0000

    made executable

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -6,4 +6,10 @@ const getImmutableObject = (object) => {
   return fromJS(object);
 };
 
-getImmutableObject();
+const testObj = {
+     fear: true,
+     smell: -1033575916.9145899,
+     wall: false,
+     thing: -914767132
+
+getImmutableObject(testObj);
