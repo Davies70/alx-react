d532b722cdacc9041a1bd305ee6cc8b8285f0ca4
Ajayi Davies <ajayidavies7@gmail.com>
2023-03-23 23:21:26 +0100

    import refused to work

--- 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -1,20 +1,12 @@
-import * as notificationsData from '../../../../notifications.json' assert { type: 'json' };
+const notificationsData = require('../../../../notifications.json');
 
-// const notificationsData = require('../../../../notifications.json');
-const notificationsD = [notificationsData];
-console.log(notificationsD);
-
-// function getAllNotificationsByUser(userId) {
-//   const context = notificationsD
-//     .filter((notification) => {
-//       return notification.author.id === userId;
-//     })
-//     .map((notification) => {
-//       return notification.context;
-//     });
-
-//   console.log(context);
-// }
-
-// const id = '5debd764a7c57c7839d722e9';
-// getAllNotificationsByUser(id);
+function getAllNotificationsByUser(userId) {
+  const context = notificationsData
+    .filter((notification) => {
+      return notification.author.id === userId;
+    })
+    .map((notification) => {
+      return notification.context;
+    });
+  return context;
+}
