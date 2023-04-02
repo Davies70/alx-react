9148dd34ab01be1eb5618ba6c6fb7758c6d73d67
Ajayi Davies <ajayidavies7@gmail.com>
2023-04-02 19:44:42 +0200

    Store created

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createStore } from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
